843293eb3bc78eb2924a0b36747f471ecdea9e3e
Pandey <pandvike@38f9d3698f00.ant.amazon.com>
2020-08-19 17:24:59 +0200

    ui template in place

---
+++ app/src/main/java/com/example/androidinferenceexample/classifier/XGBoostDLRClassifier.java
@@ -0,0 +1,32 @@
+package com.example.androidinferenceexample.classifier;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+public class XGBoostDLRClassifier {
+
+//    private static final Logger LOGGER = Logger.getLogger("XGBoostDLRClassifier");
+//
+//    private static String DEVICE_TYPE = "CPU";
+//
+//
+//    @RequiresApi(api = Build.VERSION_CODES.O)
+//    public void main() throws IOException {
+//        try(Stream<String> stream = Files.lines(Paths.get("libs/sample"))){
+//                stream.forEach(System.out::println);
+//        }
+//
+//    }
+
+
+
+
+
+}
