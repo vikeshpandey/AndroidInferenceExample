a31580fd24504c6e87f1a1377fead28d0243cebc
Pandey <pandvike@38f9d3698f00.ant.amazon.com>
2020-08-26 12:47:10 +0200

    working copy

--- app/src/main/java/com/example/androidinferenceexample/DisplayResultActivity.java
+++ app/src/main/java/com/example/androidinferenceexample/DisplayResultActivity.java
@@ -1,13 +1,21 @@
 package com.example.androidinferenceexample;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.amazon.neo.dlr.DLR;
+
+import java.util.Arrays;
 
 public class DisplayResultActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,11 +23,90 @@ public class DisplayResultActivity extends AppCompatActivity {
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        float[] inputArr = intent.getFloatArrayExtra(MainActivity.INPUT_ARR);
 
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textViewResult);
-        textView.setText(message);
+        long handle = loadModel(); //TODO:: fix the model path by placing the model into libs folder
+
+        // This is an optional method, comment it out if not needed
+        logModelMetadata(handle);
+
+        final float result = performInference(handle, inputArr);
+
+
+        final TextView textView = findViewById(R.id.textViewResult);
+        final String resultString = Arrays.toString(inputArr) + " -> " + result;
+
+        //view the result on the screen
+        textView.setText(resultString);
     }
+
+    private long loadModel() {
+        //TODO:: fix the model path by placing the model in libs
+        long handle = DLR.CreateDLRModel("fullModelPath", 1, 0);
+        Log.i("DLR", "CreateDLRModel: " + handle);
+        if (handle == 0) {
+            Log.i("DLR", "DLRGetLastError: " + DLR.DLRGetLastError());
+            throw new RuntimeException("CreateDLRModel failed");
+        }
+        return handle;
+    }
+
+    private void logModelMetadata(long handle) {
+        Log.i("DLR", "GetDLRBackend: " + DLR.GetDLRBackend(handle));
+        Log.i("DLR", "GetDLRNumInputs: " + DLR.GetDLRNumInputs(handle));
+        Log.i("DLR", "GetDLRNumWeights: " + DLR.GetDLRNumWeights(handle));
+        Log.i("DLR", "GetDLRNumOutputs: " + DLR.GetDLRNumOutputs(handle));
+
+        String inputName = DLR.GetDLRInputName(handle, 0);
+        Log.i("DLR", "GetDLRInputName[0]: " + inputName);
+        Log.i("DLR", "GetDLRWeightName[4]: " + DLR.GetDLRWeightName(handle, 4));
+
+        int outDim = DLR.GetDLROutputDim(handle, 0);
+        long outSize = DLR.GetDLROutputSize(handle, 0);
+        Log.i("DLR", "GetDLROutputSize[0]: " + outSize);
+        Log.i("DLR", "GetDLROutputDim[0]: " + outDim);
+        //GetDLROutputShape
+        long[] out_shape = new long[outDim];
+        if (DLR.GetDLROutputShape(handle, 0, out_shape) != 0) {
+            Log.i("DLR", "DLRGetLastError: " + DLR.DLRGetLastError());
+            throw new RuntimeException("GetDLROutputShape failed");
+        }
+        Log.i("DLR", "GetDLROutputShape[0]: " + Arrays.toString(out_shape));
+    }
+
+
+    private float performInference(long handle, float[] inputArr) {
+        setModelInput(handle, inputArr);
+        runModel(handle);
+        return getInferenceResult(handle);
+    }
+
+    private void setModelInput(long handle, float[] inputArr) {
+        long[] shape = {1, 4}; // hard coded as of now
+
+        if (DLR.SetDLRInput(handle, "data", shape, inputArr, 2) != 0) {
+            Log.i("DLR", "DLRGetLastError: " + DLR.DLRGetLastError());
+            throw new RuntimeException("SetDLRInput failed");
+        }
+        Log.i("DLR", "SetDLRInput: OK");
+    }
+
+    private void runModel(long handle) {
+        if (DLR.RunDLRModel(handle) != 0) {
+            Log.i("DLR", "DLRGetLastError: " + DLR.DLRGetLastError());
+            throw new RuntimeException("RunDLRModel failed");
+        }
+        Log.i("DLR", "RunDLRModel: OK");
+    }
+
+    private float getInferenceResult(long handle) {
+        float[] output = {0};
+        if (DLR.GetDLROutput(handle, 0, output) != 0) {
+            Log.i("DLR", "DLRGetLastError: " + DLR.DLRGetLastError());
+            throw new RuntimeException("GetDLROutput failed");
+        }
+        return output[0];
+    }
+
 
 }
--- app/src/main/java/com/example/androidinferenceexample/MainActivity.java
+++ app/src/main/java/com/example/androidinferenceexample/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.androidinferenceexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.androidinferenceexample.MESSAGE";
+    public static final String INPUT_ARR = "com.example.androidinferenceexample.editTextInput";
 
 
     @Override
@@ -18,13 +20,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /** Called when the user taps the Send button */
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayResultActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editTextInput);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        EditText editText = findViewById(R.id.editTextInput);
+        String inputText = editText.getText().toString();
+        intent.putExtra(INPUT_ARR, convertStringToFloatArr(inputText));
 
+        startActivity(intent);
     }
 
+    private static float[] convertStringToFloatArr(String inputText) {
+        final String[] stringArr = inputText.split(",");
+        final int arrLength = stringArr.length;
+        final float[] floatArr = new float[arrLength];
+        for (int i = 0; i< arrLength; i++){
+            floatArr[i] = Float.parseFloat(stringArr[i]);
+        }
+        return floatArr;
+    }
 }
